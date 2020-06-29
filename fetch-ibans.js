84b8c82b02ac50eece28c35b78a0e503e2195334
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-06-29 11:41:47 +0200

    fetch-ibans: fixed IBAN cleaning

--- fetch-ibans.js
+++ fetch-ibans.js
@@ -4,17 +4,32 @@ const fetch = require('node-fetch')
 
 const ibanPattern = /[A-Z]{2} ?[0-9]{2} ?[A-Z0-9 ]{4,}/
 
-async function cleanIban (iban) {
-  return iban.toUpperCase().replace(/[\W_]/g, '')
+/**
+ * Callback that makes unique the items of an array.
+ *
+ * Example usage:
+ *   var myArray = ['a', 1, 'a'];
+ *   var myUniqueArray = myArray.filter(onlyUnique);
+ *
+ * @param arrayItem one of the items of the array
+ * @param {number} arrayIndex
+ * @param {Array} arrayToMakeUnique
+ *
+ * @returns {boolean}
+ *
+ * @see https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+ */
+function onlyUnique (arrayItem, arrayIndex, arrayToMakeUnique) {
+  const arrayOfJsonizedItems = arrayToMakeUnique.map(JSON.stringify)
+  const jsonizedItem = JSON.stringify(arrayItem)
+  return arrayOfJsonizedItems.indexOf(jsonizedItem) === arrayIndex
 }
 
 /**
- * @see https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+ * @param {Array<string>} urls
+ *
+ * @returns {Promise<Array<string>>}
  */
-function onlyUnique (value, index, self) {
-  return self.indexOf(value) === index
-}
-
 module.exports = function (urls) {
   return Promise.all(urls.map(url => fetch(url)))
     .then(responses => Promise.all(responses.map(response => response.text()))
@@ -22,8 +37,8 @@ module.exports = function (urls) {
       .then(bodies => bodies.map(body => body.match(new RegExp(ibanPattern, 'g'))))
     )
   // Clean raw IBANs within each array.
-    .then(ibanArrays => ibanArrays.map(ibanArray => ibanArray.map(iban => cleanIban(iban))))
-  // Merge all IBAN arrays, keep only unique values and sort.
+    .then(ibanArrays => ibanArrays.map(ibanArray => ibanArray.map(iban => iban.toUpperCase().replace(/[\W_]/g, ''))))
+  // Merge all IBAN arrays, keep only unique values and sort items.
     .then(ibanArrays => [].concat(...ibanArrays).filter(onlyUnique).sort())
     .catch(error => console.error(error))
 }
