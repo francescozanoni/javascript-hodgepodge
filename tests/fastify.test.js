e4e05d96b7c3952c22a66d6b81ae3f273c97fb33
Francesco Zanoni <francescozanoni81@gmail.com>
2020-10-19 19:26:52 +0200

    continued Fastify and nide-fetch tests

--- tests/fastify.test.js
+++ tests/fastify.test.js
@@ -1,4 +1,5 @@
 const fastify = require('fastify')
+const fetch = require('node-fetch')
 
 function buildApp (opts = {}) {
   const app = fastify(opts)
@@ -9,8 +10,13 @@ function buildApp (opts = {}) {
   return app
 }
 
-test('Fastify HTTP server', async () => {
-  const app = buildApp()
+let app
+
+beforeEach(() => app = buildApp())
+afterEach(() => app.close())
+
+describe('Testing Fastify', () => {
+test('With HTTP injection', async () => {
   const request = {
     method: 'GET',
     url: '/'
@@ -24,4 +30,19 @@ test('Fastify HTTP server', async () => {
     .toStrictEqual('{"hello":"world"}')
 })
 
+test('With running server', async () => {
+
+  app.listen(0, (error) => {
+
+    const response = await fetch('http://localhost')
+    const body = await response.text()
+
+    expect(response.status)
+    .toStrictEqual(200)
+  expect(body)
+    .toStrictEqual('{"hello":"world"}')
+})
+})
+
+// https://jestjs.io/docs/en/setup-teardown
 // Reference: https://www.fastify.io/docs/latest/Testing
