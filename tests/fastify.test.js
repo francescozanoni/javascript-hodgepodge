f5810bb7350507ee326209b11de3e884b35ab307
Francesco Zanoni <francescozanoni81@gmail.com>
2020-10-19 21:08:04 +0200

    continued Fastift test example

--- tests/fastify.test.js
+++ tests/fastify.test.js
@@ -1,6 +1,11 @@
 const fastify = require('fastify')
 const fetch = require('node-fetch')
 
+/*
+https://jestjs.io/docs/en/setup-teardown
+Reference: https://www.fastify.io/docs/latest/Testing
+*/
+
 function buildApp (opts = {}) {
   const app = fastify(opts)
   app.get('/', async (request, reply) => {
@@ -12,37 +17,36 @@ function buildApp (opts = {}) {
 
 let app
 
-beforeEach(() => app = buildApp())
+beforeEach(() => { app = buildApp() })
 afterEach(() => app.close())
 
 describe('Testing Fastify', () => {
-test('With HTTP injection', async () => {
-  const request = {
-    method: 'GET',
-    url: '/'
-  }
+  test('With HTTP injection', async () => {
+    const request = {
+      method: 'GET',
+      url: '/'
+    }
 
-  const response = await app.inject(request)
+    const response = await app.inject(request)
 
-  expect(response.statusCode)
-    .toStrictEqual(200)
-  expect(response.body)
-    .toStrictEqual('{"hello":"world"}')
+    expect(response.statusCode)
+      .toStrictEqual(200)
+    expect(response.body)
+      .toStrictEqual('{"hello":"world"}')
+  })
+
+  test('With running server', async () => {
+    app.listen(3000, async (error) => {
+      if (error) {
+        return
+      }
+      const response = await fetch('http://localhost:3000')
+      const body = await response.text()
+
+      expect(response.status)
+        .toStrictEqual(200)
+      expect(body)
+        .toStrictEqual('{"hello":"world"}')
+    })
+  })
 })
-
-test('With running server', async () => {
-
-  app.listen(0, (error) => {
-
-    const response = await fetch('http://localhost')
-    const body = await response.text()
-
-    expect(response.status)
-    .toStrictEqual(200)
-  expect(body)
-    .toStrictEqual('{"hello":"world"}')
-})
-})
-
-// https://jestjs.io/docs/en/setup-teardown
-// Reference: https://www.fastify.io/docs/latest/Testing
