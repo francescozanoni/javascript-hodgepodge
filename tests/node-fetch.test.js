2d8f4692bbbc45be9d44b2ab3f988e667b5cc738
Francesco Zanoni <francescozanoni81@gmail.com>
2020-10-19 19:00:01 +0200

    improved node-fetch test examplr@

--- tests/node-fetch.test.js
+++ tests/node-fetch.test.js
@@ -1,10 +1,35 @@
 const fetch = require('node-fetch')
 
-test('node-fetch to GitHub', async () => {
-  const response = await fetch('https://github.com/')
-  const body = await response.text()
+describe('node-fetch', () => {
+  test('await', async () => {
+    const response = await fetch('https://github.com/')
+    const body = await response.text()
 
-  expect(response.status).toBe(200)
+    expect(response.status).toBe(200)
 
-  expect(body.length).toBeGreaterThan(0)
+    expect(body.length).toBeGreaterThan(0)
+  })
+
+  test('return expect resolves', () => {
+    expect.hasAssertions()
+    return expect(fetch('https://github.com/'))
+      .resolves
+      .toBeDefined()
+  })
+
+  test('await expect', async () => {
+    expect.hasAssertions()
+    await expect(fetch('https://github.com/'))
+      .resolves
+      .toBeDefined()
+  })
+
+  test('await fetch', async () => {
+    expect.hasAssertions()
+    const response = await fetch('https://github.com/')
+    expect(response)
+      .toBeDefined()
+  })
 })
+
+// Source: https://jestjs.io/docs/en/tutorial-async
