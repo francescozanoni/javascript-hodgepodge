7c70cdd6d49c53557e438c069fc65e1bffb5c329
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-11-14 12:33:05 +0100

    ESLint: customized configuration for ReactJS and JSX

--- src/react/script.js
+++ src/react/script.js
@@ -1,46 +1,52 @@
-
-
 class List extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       name: props.name,
       items: ['a', 'b', 'c']
-    };
-    this.del = this.del.bind(this);
+    }
+    this.del = this.del.bind(this)
   }
-  del(item) {
+
+  del (item) {
     const newItems = this.state.items
-              .filter(i => i !== item)
+      .filter(i => i !== item)
     this.setState({
       items: newItems
-    });
+    })
   }
-  render() {
+
+  render () {
     const action = this.del
     return <React.Fragment>
-           <h1>{this.state.name}</h1>
-           <ul>
-           {this.state.items
-              .map(i => <Item name={i} 
-                         action={action}/>)}
-           </ul>
-           </React.Fragment>;
+      <h1>{this.state.name}</h1>
+      <ul>
+        {this.state.items.map(i => <Item key={i} action={action}/>)}
+      </ul>
+    </React.Fragment>
   }
 }
 
-function Item(props) {
-  const name = props.name
+List.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+function Item (props) {
+  const key = props.key
   const action = props.action
   return <li>
-         <button onClick={() => action(name)}>
-           {name}
-         </button>
-         </li>;
+    <button onClick={() => action(key)}>
+      {key}
+    </button>
+  </li>
+}
+
+Item.propTypes = {
+  key: PropTypes.string.isRequired,
+  action: PropTypes.function.isRequired
 }
 
 ReactDOM.render(
-  <List name="List FZ" />,
+  <List name="List FZ"/>,
   document.getElementById('list')
-);
-
+)
