35b8c946f41f0591944f4ab067ab82f1f18751db
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2021-06-14 13:05:01 +0200

    refactored and improved example of how read text file with OS tools

--- src/read-text-file-lines.js
+++ src/read-text-file-lines.js
@@ -1,43 +1,49 @@
 const { exec } = require('child_process')
 
-const filePath = '/path/to/text/file'
-const firstLineIndex = '10'
-const lastLineIndex = '19'
+const filePath = './tree.html'
+const firstLineIndex = 10
+const lastLineIndex = 19
+const offset = firstLineIndex - 1
+const limit = lastLineIndex - firstLineIndex + 1
 
 // @todo add sed/powershell availability check
 
-exec(
-  'sed -n "' + firstLineIndex + ',' + lastLineIndex + '"p ' + filePath,
-  (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`)
-      return
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`)
-      return
-    }
-    console.log(`stdout: ${stdout}`)
+/**
+ * @param {Error} error
+ * @param {string|Buffer} stdout
+ * @param {string|Buffer} stderr
+ */
+function callback (error, stdout, stderr) {
+  if (error) {
+    console.log(`error: ${error.message}`)
+    return
   }
-)
+  if (stderr) {
+    console.log(`stderr: ${stderr}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+}
 
-exec(
-  'Get-Content ' + filePath + ' | Select-Object -First ' + firstLineIndex + ' -Last ' + lastLineIndex,
-  { shell: 'powershell.exe' },
-  (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`)
-      return
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`)
-      return
-    }
-    console.log(`stdout: ${stdout}`)
-  }
-)
+if (process.platform.startsWith('win') === true) {
+  exec(
+    'Get-Content ' + filePath + ' | Select-Object -Skip ' + offset + ' -First ' + limit,
+    { shell: 'powershell.exe' },
+    callback
+  )
+} else {
+  exec(
+    'sed -n "' + firstLineIndex + ',' + lastLineIndex + '"p ' + filePath,
+    callback
+  )
+}
 
 // References:
 // - https://stackabuse.com/executing-shell-commands-with-node-js/
 // - https://stackoverflow.com/questions/10179114/execute-powershell-script-from-node-js
 // - https://stackoverflow.com/questions/14759649/how-to-print-a-certain-line-of-a-file-with-powershell
+// - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-object
+
+// Unused references:
+// - https://github.com/npocmaka/batch.scripts/blob/master/fileUtils/tailHead.bat (Windows without PowerShell)
+// - https://gist.github.com/jmptable/7a3aa580efffdef50fa9f0dd3d068d6f (cmd-exists.js)
